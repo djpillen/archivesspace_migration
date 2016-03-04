2b0f279f58841b9a65944212f6afbee31951b3b7
djpillen@umich.edu <djpillen@umich.edu>
2016-03-04 12:40:46 -0500

    making notes from unittitles

--- unitdate_unittitle_fix_post/make_odds_from_unittitles.py
+++ unitdate_unittitle_fix_post/make_odds_from_unittitles.py
@@ -7,20 +7,21 @@ def make_odds_from_unittitles(ead_dir):
 	for filename in os.listdir(ead_dir):
 		print "Making <odd>s from unittitles in {0}".format(filename)
 		tree = etree.parse(join(ead_dir,filename))
-		unittitles = tree.xpath('//did/unittitle')
+		components = tree.xpath("//dsc//*[starts-with(local-name(), 'c0')]")
 		rewrite = False
-		for unittitle in unittitles:
-			if unittitle.text:
-				unittitle_text = unittitle.text.strip().encode('utf-8')
-				if unittitle_text.startswith('(') and unittitle_text.endswith(')') and len(re.findall(r'[\(\)]',unittitle_text)) == 2:
-					rewrite = True
-					new_odd = etree.Element('odd')
-					new_p = etree.Subelement(new_odd,'p')
-					new_p.text = unittitle_text
-					did = unitittle.getparent()
-					component = did.getparent()
-					component.insert(component.index(did)+1,new_odd)
-					did.remove(unittitle)
+		for component in components:
+			if component.xpath('./did/unittitle') and component.xpath('./did/unitdate'):
+				unittitle = component.xpath('./did/unittitle')[0]
+				if unittitle.text:
+					unittitle_text = unittitle.text.strip().encode('utf-8')
+					if unittitle_text.startswith('(') and unittitle_text.endswith(')') and len(re.findall(r'[\(\)]',unittitle_text)) == 2:
+						rewrite = True
+						new_odd = etree.Element('odd')
+						new_p = etree.SubElement(new_odd,'p')
+						new_p.text = unittitle_text
+						did = unittitle.getparent()
+						component.insert(component.index(did)+1,new_odd)
+						did.remove(unittitle)
 		if rewrite:
 			with open(join(ead_dir,filename),'w') as f:
 				f.write(etree.tostring(tree,encoding='utf-8',xml_declaration=True,pretty_print=True))
